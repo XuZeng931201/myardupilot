91e0fb7de7a3bf8f36528c4b882bc0cf75df59e8
zengxuzengxu <492462168@qq.com>
2019-04-07 10:31:58 +0800

    param set shot_takeoff_release_ms default to 0

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -1200,7 +1200,7 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
 
 	AP_GROUPINFO("XR_SHOT_TRIM_CD", 20, ParametersG2, shot_takeoff_trim_angle_cdeg, 0),
 
-	AP_GROUPINFO("XR_SHOT_TK_MS", 21, ParametersG2, shot_takeoff_release_ms, 3000),
+	AP_GROUPINFO("XR_SHOT_TK_MS", 21, ParametersG2, shot_takeoff_release_ms, 0),
 
     AP_GROUPEND
 };
